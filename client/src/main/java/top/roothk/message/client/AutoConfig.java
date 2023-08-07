e76a631d8e5c7314f77ebab714391aa1c788a45e
hongkeng <hk374790498@gmail.com>
2023-08-07 15:55:34 +0800

    Init Project

--- client/src/main/java/top/roothk/message/client/AutoConfig.java
+++
@@ -1,9 +0,0 @@
-package top.roothk.message.client;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
-
-@EnableConfigurationProperties({ServerProperties.class})
-@ComponentScan({"top.roothk.message.client.**"})
-public class AutoConfig {
-}
